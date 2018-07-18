79e06c1ed5d383a9b299219a7454019f9441f1a3
Stephan Hoyer <ste.hoyer@gmail.com>
2018-07-18 12:08:08 +0200

    support components - fixes #6

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-function noop(){}
+function noop() {}
 
 module.exports = function(h) {
-  return new Proxy({}, {
-    get: function(_, tagName) {
-      return new Proxy(noop, {
-        apply(_, __, args) {
-          return h(tagName, [], ...args)
-        },
-        get(_, className) {
-          const classNames = [className]
-          const proxy = new Proxy(noop, {
-            get(_, className) {
-              classNames.push(className)
-              return proxy
-            },
-            apply(_, ___, args) {
-              return h(tagName, classNames, ...args)
-            }
-          })
-          return proxy
-        }
-      })
+  return new Proxy(
+    {},
+    {
+      set: function(components, componentName, component) {
+        components[componentName] = component
+      },
+      get: function(components, tagName) {
+        tagName = components[tagName] || tagName
+        return new Proxy(noop, {
+          apply(_, __, args) {
+            return h(tagName, [], ...args)
+          },
+          get(_, className) {
+            const classNames = [className]
+            const proxy = new Proxy(noop, {
+              get(_, className) {
+                classNames.push(className)
+                return proxy
+              },
+              apply(_, ___, args) {
+                return h(tagName, classNames, ...args)
+              },
+            })
+            return proxy
+          },
+        })
+      },
     }
-  })
+  )
 }
--- test.js
+++ test.js
@@ -1,24 +1,56 @@
 const o = require('ospec')
 const tagl = require('.')
 
-o('basic usage', function(done) {
-  function h(tagname, classes, ...args) {
-    o(tagname).equals('div')
-    o(classes).deepEquals([])
-    o(args).deepEquals([1, 2])
-    done()
-  }
-  const { div } = tagl(h)
-  div(1, 2)
+o.spec('basic usage', () => {
+  o('without classes', function(done) {
+    function h(tagname, classes, ...args) {
+      o(tagname).equals('div')
+      o(classes).deepEquals([])
+      o(args).deepEquals([1, 2])
+      done()
+    }
+    const { div } = tagl(h)
+    div(1, 2)
+  })
+
+  o('with classes', function(done) {
+    function h(tagname, classes, ...args) {
+      o(tagname).equals('span')
+      o(classes).deepEquals(['foo'])
+      o(args).deepEquals([{}, 'huhu', 'haha'])
+      done()
+    }
+    const { span } = tagl(h)
+    span.foo({}, 'huhu', 'haha')
+  })
 })
 
-o('basic usage', function(done) {
-  function h(tagname, classes, ...args) {
-    o(tagname).equals('span')
-    o(classes).deepEquals(['foo'])
-    o(args).deepEquals([{}, 'huhu', 'haha'])
-    done()
-  }
-  const { span } = tagl(h)
-  span.foo({}, 'huhu', 'haha')
+o.spec('components', () => {
+  o('without classes', function(done) {
+    function myComponent() {}
+    function h(tagname, classes, ...args) {
+      o(tagname).equals(myComponent)
+      o(classes).deepEquals([])
+      o(args).deepEquals([1, 2])
+      done()
+    }
+    t = tagl(h)
+    t.myCmp = myComponent
+    const { myCmp } = t
+    myCmp(1, 2)
+  })
+
+  o('with classes', function(done) {
+    function myComponent() {}
+    function h(tagname, classes, ...args) {
+      o(tagname).equals(myComponent)
+      o(classes).deepEquals(['foo'])
+      o(args).deepEquals([{}, 'huhu', 'haha'])
+      done()
+    }
+    t = tagl(h)
+    t.myCmp = myComponent
+    const { myCmp } = t
+    myCmp.foo({}, 'huhu', 'haha')
+  })
 })
