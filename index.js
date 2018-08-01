96034e885adecae6b0958c86614f222252fd7263
Stephan Hoyer <ste.hoyer@gmail.com>
2018-08-01 13:01:25 +0200

    Support local components

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 function noop() {}
 
 module.exports = function(h) {
-  return new Proxy(
-    {},
-    {
-      get: function(components, tagName) {
-        tagName = components[tagName] || tagName
-        return new Proxy(noop, {
-          apply(_, __, args) {
-            return h(tagName, [], ...args)
-          },
+  function createProxy(tagName) {
+    return new Proxy(noop, {
+      apply(_, __, args) {
+        return h(tagName, [], ...args)
+      },
+      get(_, className) {
+        const classNames = [className]
+        const proxy = new Proxy(noop, {
           get(_, className) {
-            const classNames = [className]
-            const proxy = new Proxy(noop, {
-              get(_, className) {
-                classNames.push(className)
-                return proxy
-              },
-              apply(_, ___, args) {
-                return h(tagName, classNames, ...args)
-              },
-            })
+            classNames.push(className)
             return proxy
           },
+          apply(_, ___, args) {
+            return h(tagName, classNames, ...args)
+          },
         })
+        return proxy
       },
-    }
-  )
+    })
+  }
+
+  return new Proxy(noop, {
+    apply: (_, __, [component]) => createProxy(component),
+    get: (components, tagName) => createProxy(components[tagName] || tagName),
+  })
 }
--- test.js
+++ test.js
@@ -25,7 +25,7 @@ o.spec('basic usage', () => {
   })
 })
 
-o.spec('components', () => {
+o.spec('global components', () => {
   o('without classes', function(done) {
     function myComponent() {}
     function h(tagname, classes, ...args) {
@@ -51,6 +51,34 @@ o.spec('components', () => {
     t = tagl(h)
     t.myCmp = myComponent
     const { myCmp } = t
+    myCmp.foo({}, 'huhu', 'haha')
+  })
+})
+
+o.spec('local components', () => {
+  o('without classes', function(done) {
+    function myComponent() {}
+    function h(tagname, classes, ...args) {
+      o(tagname).equals(myComponent)
+      o(classes).deepEquals([])
+      o(args).deepEquals([1, 2])
+      done()
+    }
+    t = tagl(h)
+    const myCmp = t(myComponent)
+    myCmp(1, 2)
+  })
+
+  o('with classes', function(done) {
+    function myComponent() {}
+    function h(tagname, classes, ...args) {
+      o(tagname).equals(myComponent)
+      o(classes).deepEquals(['foo'])
+      o(args).deepEquals([{}, 'huhu', 'haha'])
+      done()
+    }
+    t = tagl(h)
+    const myCmp = t(myComponent)
     myCmp.foo({}, 'huhu', 'haha')
   })
 })
